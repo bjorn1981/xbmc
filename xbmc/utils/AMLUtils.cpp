3a25e9d46a9392a4635100485edb8c7ac88e10e8
Brandon McAnsh <brandonm@matricom.net>
2014-08-09 00:22:53 -0400

    AMLUtils: Limit set_audio_passthrough to <= m8
    Signed-off-by: Brandon McAnsh <brandonm@matricom.net>

--- xbmc/utils/AMLUtils.cpp
+++ xbmc/utils/AMLUtils.cpp
@@ -218,7 +218,9 @@ void aml_cpufreq_max(bool limit)
 
 void aml_set_audio_passthrough(bool passthrough)
 {
-  if (aml_present())
+  if ( aml_present()
+    && aml_get_device_type() != AML_DEVICE_TYPE_UNKNOWN
+    && aml_get_device_type() <= AML_DEVICE_TYPE_M8)
   {
     // m1 uses 1, m3 and above uses 2
     int raw = aml_get_device_type() == AML_DEVICE_TYPE_M1 ? 1 : 2;
